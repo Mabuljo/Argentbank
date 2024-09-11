ca20fafc9ef630fbba3b8e4bb35a47767e8795f4
Mabuljo <151923541+Mabuljo@users.noreply.github.com>
2024-09-11 08:09:41 +0200

    Initialisation du projet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./assets/main.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Signin from "./pages/Signin";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import bankLogo from '../assets/img/argentBankLogo.png';
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <header>
+            <nav className="main-nav">
+                <NavLink to="/" className="main-nav-logo">
+                    <img
+                    className="main-nav-logo-image"
+                    src={bankLogo}
+                    alt="Argent Bank Logo"
+                    />
+                    <h1 className="sr-only">Argent Bank</h1>
+                </NavLink>
+                <div>
+                    <NavLink to="/signin" className="main-nav-item">
+                        <i className="fa fa-user-circle"></i>
+                        Sign In
+                    </NavLink>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
