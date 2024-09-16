29f7bf6c6008830d1ee0cda93f4efb9f8b33dd17
Mabuljo <151923541+Mabuljo@users.noreply.github.com>
2024-09-16 11:39:09 +0200

    initialisation du store et de la 1ere Slice

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import bankLogo from '../assets/img/argentBankLogo.webp';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
         <header>
             <nav className="main-nav">
-                <Link to="/" className="main-nav-logo">
+                <NavLink to="/" className="main-nav-logo">
                     <img
                     className="main-nav-logo-image"
                     src={bankLogo}
                     alt="Argent Bank Logo"
                     />
                     <h1 className="sr-only">Argent Bank</h1>
-                </Link>
+                </NavLink>
                 <div>
-                    <Link to="/signin" className="main-nav-item">
+                    <NavLink to="/signin" className="main-nav-item">
                         <i className="fa fa-user-circle"></i>
                         Sign In
-                    </Link>
+                    </NavLink>
                 </div>
             </nav>
         </header>
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const Signin = () => {
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+       
+    };
+
     return (
         <main className="main bg-dark">
             <section className="sign-in-content">
                 <i className="fa fa-user-circle sign-in-icon"></i>
                 <h1>Sign In</h1>
-                <form>
+                <form on onSubmit={handleSubmit}>
                     <div className="input-wrapper">
                         <label htmlFor="username">Username</label>
-                        <input type="text" id="username" />
+                        <input type="text" id="username" required value={username} onChange={(e) =>setUserName(e.target.value)} />
                     </div>
                     <div className="input-wrapper">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" />
+                        <input type="password" id="password" required value={password} onChange={(e) =>setPassword(e.target.value)} />
                     </div>
                     <div className="input-remember">
                         <input type="checkbox" id="remember-me" />
