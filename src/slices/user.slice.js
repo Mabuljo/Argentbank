30409c82091ebeaf47cfacc14745db63daa45f87
Mabuljo <151923541+Mabuljo@users.noreply.github.com>
2024-09-17 10:02:56 +0200

    essai de la connexion user

--- src/slices/user.slice.js
+++ src/slices/user.slice.js
@@ -3,8 +3,8 @@ import { createSlice } from "@reduxjs/toolkit";
 export const userSlice = createSlice({
     name: 'user',
     initialState: {
-      user: null,
-      token: null
+      user: '',
+      token: ''
     },
     reducers :{}
 })
