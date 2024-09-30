bf469b6524ceb7419ce30359a5101650e51e068d
Mabuljo <151923541+Mabuljo@users.noreply.github.com>
2024-09-30 19:02:17 +0200

    minification du fichier CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./assets/main.css";
+import "./assets/main.min.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/store";
